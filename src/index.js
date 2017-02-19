2f69d7fc27fe4258309d184422083b584fa7381b
shpakkdv <shpak_kdv@mail.ru>
2017-02-19 22:18:49 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function zeros(expression) {
-  // your solution
+	let array = expression.split('*'), two = 0, five = 0;
+	for (let i = 0; i < array.length; i++) {
+
+		if (array[i][array[i].length-1] === '!' && array[i][array[i].length-2] === '!') {
+			let current = parseInt(array[i]);
+			if (current % 2) {
+				for (let j = 5; j <= current; j+=10) {
+					five += findAmount(j, 5);
+				}
+			} else {
+				for (let j = 2; j <= current; j+=2) {
+					if (!(j % 10)) {
+						two += findAmount(j, 2);
+						five += findAmount(j, 5);
+					} else {
+						two += findAmount(j, 2);
+					}
+				}
+			}
+		} else {
+			for (let j = 2; j <= parseInt(array[i]); j++) {
+				if (!(j % 10)) {
+					two += findAmount(j, 2);
+					five += findAmount(j, 5);
+				} else if (!(j % 5)) {
+					five += findAmount(j, 5);
+				} else if (!(j % 2)) {
+					two += findAmount(j, 2);
+				} 
+			}
+		}
+	}
+	return Math.min(two, five);
+}
+
+function findAmount(number, element) {
+	let temp = number, count = 0;
+	while (temp >= element && !(temp % element)) {
+		count++;
+		temp /= element;
+	}
+	return count;
 }
